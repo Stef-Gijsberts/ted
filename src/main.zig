7be22084a47af68318df4dfdf0b1bafc073a65fc
Stef Gijsberts <55096788+Stef-Gijsberts@users.noreply.github.com>
2022-08-02 22:47:53 +0200

    Refactor entering and exiting 'raw mode'

--- src/main.zig
+++ src/main.zig
@@ -2,18 +2,37 @@ const std = @import("std");
 const os = std.os;
 const linux = std.os.linux;
 
-pub fn main() anyerror!void {
-    // based on the enableRawMode function from Kilo, which is written by
-    // Salvatore Sanfilippo aka antirez, licensed under the BSD 2 clause
-    // license.
-    // https://github.com/antirez/kilo/blob/69c3ce609d1e8df3956cba6db3d296a7cf3af3de/kilo.c#L218,
-    const old = try os.tcgetattr(os.STDIN_FILENO);
-    var raw = old;
-    raw.iflag &= ~(linux.BRKINT | linux.ICRNL | linux.INPCK | linux.ISTRIP | linux.IXON);
-    raw.oflag &= ~(linux.OPOST);
-    raw.cflag |= (linux.CS8);
-    raw.lflag &= (linux.ECHO | linux.ICANON | linux.IEXTEN | linux.ISIG);
+const raw_mode = struct {
+    var maybe_original: ?os.termios = null;
 
-    try os.tcsetattr(os.STDIN_FILENO, os.TCSA.FLUSH, raw);
-    defer os.tcsetattr(os.STDIN_FILENO, os.TCSA.FLUSH, old) catch {};
+    fn enter() !void {
+        // This code is based on the enableRawMode() function from Kilo, which
+        // is written by Salvatore Sanfilippo aka antirez, licensed under the
+        // BSD 2 clause license.
+        //
+        // The enableRawMode() function can be found on GitHub:
+        // https://github.com/antirez/kilo/blob/69c3ce609d1e8df3956cba6db3d296a7cf3af3de/kilo.c#L218,
+
+        const original = try os.tcgetattr(os.STDIN_FILENO);
+        maybe_original = original;
+
+        var raw = original;
+        raw.iflag &= ~(linux.BRKINT | linux.ICRNL | linux.INPCK | linux.ISTRIP | linux.IXON);
+        raw.oflag &= ~(linux.OPOST);
+        raw.cflag |= (linux.CS8);
+        raw.lflag &= (linux.ECHO | linux.ICANON | linux.IEXTEN | linux.ISIG);
+
+        try os.tcsetattr(os.STDIN_FILENO, os.TCSA.FLUSH, raw);
+    }
+
+    fn exit() void {
+        if (maybe_original) |original| {
+            os.tcsetattr(os.STDIN_FILENO, os.TCSA.FLUSH, original) catch {};
+        }
+    }
+};
+
+pub fn main() anyerror!void {
+    try raw_mode.enter();
+    defer raw_mode.exit();
 }
