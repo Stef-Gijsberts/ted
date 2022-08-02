2f4c09591297cd8b195600d097d1da2449d98035
Stef Gijsberts <55096788+Stef-Gijsberts@users.noreply.github.com>
2022-08-02 22:59:12 +0200

    Add a small render loop

--- src/main.zig
+++ src/main.zig
@@ -35,4 +35,22 @@ const raw_mode = struct {
 pub fn main() anyerror!void {
     try raw_mode.enter();
     defer raw_mode.exit();
+
+    const in = std.io.getStdIn();
+    const out = std.io.getStdOut();
+
+    var key: [1]u8 = undefined;
+
+    while (true) {
+        // Clear screen
+        _ = try out.write("\x1B[1J");
+        
+        // Read a character
+        _ = try in.read(&key);
+
+        // Close when ctrl+c is pressed
+        if (key[0] == 3) {
+            break;
+        }
+    }
 }
